29b702250d70114bf824563c68831d7a424811e8
gengjiawen <technicalcute@gmail.com>
2017-04-20 18:59:15 +0800

    add multi tab support

--- app/src/renderer/components/LandingPageView.vue
+++ app/src/renderer/components/LandingPageView.vue
@@ -1,10 +1,76 @@
 <template>
   <div class="container">
-    <webview src="https://devdocs.io"></webview>
+    <el-tabs v-model="editableTabsValue" type="card" editable @edit="handleTabsEdit">
+      <el-tab-pane
+              v-for="(item, index) in editableTabs"
+              :key="index"
+              :label="item.title"
+              :name="item.name" >
+        <!--<version></version>-->
+        <!--{{item.url}} {{item.name}}-->
+        <webview :src="item.url"></webview>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+  import Version from './LandingPageView/Versions.vue'
+  export default {
+    components: {
+      Version
+    },
+    data () {
+      return {
+        editableTabsValue: '1',
+        editableTabs: [{
+          title: 'Devdocs',
+          name: '1',
+          url: 'https://devdocs.io'
+        }],
+        tabIndex: 1
+      }
+    },
+    mounted () {
+      console.log('mounted')
+//      this is hack for weird webview bug
+      if (this.tabIndex === 1) {
+        setTimeout(() => {
+          this.$el.querySelector('webview').reload()
+        }, 3000)
+      }
+    },
+    methods: {
+      handleTabsEdit (targetName, action) {
+        if (action === 'add') {
+          let newTabName = ++this.tabIndex + ''
+          this.editableTabs.push({
+            title: 'Devdocs',
+            name: newTabName,
+            url: 'https://devdocs.io'
+          })
+          this.editableTabsValue = newTabName
+        }
+        if (action === 'remove') {
+          let tabs = this.editableTabs
+          let activeName = this.editableTabsValue
+          if (activeName === targetName) {
+            tabs.forEach((tab, index) => {
+              if (tab.name === targetName) {
+                let nextTab = tabs[index + 1] || tabs[index - 1]
+                if (nextTab) {
+                  activeName = nextTab.name
+                }
+              }
+            })
+          }
+
+          this.editableTabsValue = activeName
+          this.editableTabs = tabs.filter(tab => tab.name !== targetName)
+        }
+      }
+    }
+  }
 </script>
 
 <style scoped>
--- app/src/renderer/main.js
+++ app/src/renderer/main.js
@@ -1,19 +1,24 @@
+import { ipcRenderer as ipc } from 'electron'
 import Vue from 'vue'
 import Electron from 'vue-electron'
-import Resource from 'vue-resource'
 import Router from 'vue-router'
-
 import App from './App'
 import routes from './routes'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
 
 Vue.use(Electron)
-Vue.use(Resource)
 Vue.use(Router)
+Vue.use(ElementUI)
 Vue.config.debug = true
 
 const router = new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes
+})
+
+ipc.on('showDebug', (event, arg) => {
+  console.log('got message')
 })
 
 /* eslint-disable no-new */
