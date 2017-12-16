b287fde2321dbd56285b6a47b732c3ddaf89b03c
gengjiawen <technicalcute@gmail.com>
2017-12-16 20:15:59 +0800

    bump dependency

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,7 +4,7 @@ import Electron from 'vue-electron'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(Electron)
 Vue.use(ElementUI)
