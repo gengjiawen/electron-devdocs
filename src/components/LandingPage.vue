337f7a2addc1ae85e3dbb444ef4adae4e81ed2ce
gengjiawen <technicalcute@gmail.com>
2019-11-10 17:32:57 +0800

    use latest node in CircleCI

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -93,7 +93,7 @@ export default {
                   `https://www.google.com/search?q=${params.selectionText}&&client=electron-devdocs-by-daniel-geng`
                 )
               },
-            }
+            },
           ],
         })
       })
